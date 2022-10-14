79001b721fbc2f2bec3b7eba9324d9cc8f38084e
Talal <talal.alothman@gmail.com>
2022-10-14 19:50:15 +0300

    done with HOME VIEW and DETAIL VIEW sections

--- products/views.py
+++ products/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def get_home(request):
     return HttpResponse("<h1>Hello, Welcome to my website!</h1>")
+
+
+def get_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return HttpResponse(
+        f"""
+                    <div><strong>Product:</strong> {product.name}</div>
+                    <div><strong>Price:</strong> {product.price}KD</div>
+                    <div><strong>Details:</strong> {product.description}</div>
+                    """
+    )
